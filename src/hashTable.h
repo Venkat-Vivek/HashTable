d635fb56689c47bdee5181a86907a4f9a52206b3
Venkat-Vivek <simhadrivenkatvivek@gmail.com>
2024-09-27 18:00:59 +0530

    resolving duplicate key insertion and same hash values for diff keys

--- src/hashTable.h
+++ src/hashTable.h
@@ -9,15 +9,17 @@ struct Node {
     K key;
     V value;
     bool isEmpty;
+    bool isDeleted;
     uint32_t hash;
     Node(){
         key = K();
         value = V();
+        isDeleted = false;
         isEmpty = true;
         hash = 0;
     }
 };
-//hash table
+
 template<typename K, typename V>
 struct HashTable {
     int cap;
@@ -52,17 +54,26 @@ struct HashTable {
         uint32_t hashedValue = getHash(key);
         uint32_t ind = hashedValue % cap;
         int val = 0;
-
-        while (val < cap && !hashTable[(ind + val) % cap].isEmpty) {
+        bool flag = false;
+        while (val < cap && (!hashTable[(ind + val) % cap].isEmpty || hashTable[(ind + val) % cap].isDeleted)) {
+            if(hashTable[(ind + val) % cap].hash == hashedValue && hashTable[(ind + val) % cap].key == key){
+                flag = true;
+                break;
+            }
             val++;
         }
-
-        hashTable[(ind + val) % cap].key = key;
-        hashTable[(ind + val) % cap].value = value;
-        hashTable[(ind + val) % cap].isEmpty = false;
-        hashTable[(ind + val) % cap].hash = hashedValue;
-        size++;
-
+        if(flag){
+            cout << "Duplicate key" << endl;
+            return;
+        }
+        else{
+            hashTable[(ind + val) % cap].key = key;
+            hashTable[(ind + val) % cap].value = value;
+            hashTable[(ind + val) % cap].isEmpty = false;
+            hashTable[(ind + val) % cap].hash = hashedValue;
+            size++;
+        }
+   
         if (size >= (int)(0.7 * cap)) {
             int tempcap = cap;
             cap *= 2;
@@ -76,7 +87,7 @@ struct HashTable {
 
         int val = 0;
         while (val < cap) {
-            if (hashTable[(ind + val) % cap].hash == hashedValue) {
+            if (hashTable[(ind + val) % cap].hash == hashedValue && hashTable[(ind + val) % cap].key == key) {
                 hashTable[(ind + val) % cap].value = value;
                 return true;
             }
@@ -88,20 +99,20 @@ struct HashTable {
     bool remove(const K& key) {
         uint32_t hashedValue = getHash(key);
         uint32_t ind = hashedValue % cap;
-
         int val = 0;
         while (val < cap) {
-            if (hashTable[(ind + val) % cap].hash == hashedValue) {
+            if (hashTable[(ind + val) % cap].hash == hashedValue && hashTable[(ind + val) % cap].key == key) {
                 hashTable[(ind + val) % cap].key = K();
                 hashTable[(ind + val) % cap].value = V();
                 hashTable[(ind + val) % cap].isEmpty = true;
+                hashTable[(ind + val) % cap].isDeleted = true;
                 hashTable[(ind + val) % cap].hash = 0;
-                size--;
-                if (size < (cap / 4) && (cap / 2 >= 16 )) {
-                    int tempcap = cap;
-                    cap /= 2;
-                    resizeTable(tempcap);
-                }
+                
+                // if (size < (cap / 4) && (cap / 2 >= 16 )) {
+                //     int tempcap = cap;
+                //     cap /= 2;
+                //     resizeTable(tempcap);
+                // }
                 return true;
             }
             val++;
@@ -115,7 +126,7 @@ struct HashTable {
 
         int val = 0;
         while (val < cap) {
-            if (hashTable[(ind + val) % cap].hash == hashedValue) {
+            if (hashTable[(ind + val) % cap].hash == hashedValue && hashTable[(ind + val) % cap].key == key) {
                 return hashTable[(ind + val) % cap].value;
             }
             val++;
@@ -130,7 +141,7 @@ struct HashTable {
         hashTable = new Node<K, V>[cap];
 
         for (int i = 0; i < oldCap; i++) {
-            if (!oldTable[i].isEmpty) {
+            if (!oldTable[i].isEmpty && !oldTable[i].isDeleted) {
                 insert(oldTable[i].key, oldTable[i].value);
             }
         }
@@ -139,8 +150,9 @@ struct HashTable {
 
     void printHashTable() {
         for (int i = 0; i < cap; ++i) {
-            cout << "Block " << i << ": " << hashTable[i].key << " => " << hashTable[i].value << endl;
+            if(!hashTable[i].isDeleted && !hashTable[i].isEmpty){
+                cout << "Block " << i << ": " << hashTable[i].key << " => " << hashTable[i].value << endl;
+            }
         }
     }
 };
-
